d29e542d8642c5dfe23d87a016ba18c88d842310
michaelb87 <mb@michaelb.at>
2018-06-11 02:45:57 +0200

    ls and status command

--- lib/api_manager.py
+++ lib/api_manager.py
@@ -1,5 +1,34 @@
 import requests
 class ApiManager():
+    files = [] # cached list of files
+
     def __init__(self, host, apikey):
-        self.host = host
-        self.apikey = apikey
+        self.headers = {'X-Api-Key' : apikey}
+        self.base_url = 'http://{}/api'.format( host.replace('http://', '') )
+    
+    def list_files(self):
+        r = requests.get(self.base_url + '/files', headers=self.headers)
+        try:
+            self.files = r.json()['files']
+            self.files.sort(key=lambda f: f['date'], reverse=False)
+            results = ['{}'.format(f['display']) for f in self.files]
+            return '\n'.join(results)
+        except Exception as e:
+            return 'Error loading files: {}'.format(e)
+    def get_file (self, filename):
+        if len(self.files) == 0:
+            self.list_files()
+        return 'TODO'
+
+    def get_status(self):
+        r = requests.get(self.base_url + '/job', headers=self.headers)
+        try:
+            resp = r.json()
+            curr_file = resp['job']['file']
+            progress = resp['progress']
+            return 'Printing {} is {} percent done. {} minutes left.'.format(
+                curr_file['display'], 
+                progress['completion'], 
+                round(float(progress['printTimeLeft'])/60,2) )
+        except Exception as e:
+            return 'Could not get current status: {}'.format(e)
--- lib/command_parser.py
+++ lib/command_parser.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 class CommandParser():
     def __init__(self, api_manager):
         self.api_manager = api_manager
@@ -5,5 +6,18 @@ class CommandParser():
     def parse_cmd(self,cmd):
         cleaned = cmd.strip()
         if cleaned == 'h' or cleaned == 'help':
-            return 'I cannot help you now'
+            return self.help_text()
+        if cleaned == 'ls' or cleaned == 'list':
+            return self.api_manager.list_files()
+        if cleaned == 'status':
+            return self.api_manager.get_status()
         return cmd
+    
+    def help_text(self):
+        return dedent(
+            '''
+            Help:
+            
+            ls - list all files
+            status - printer status
+            ''')
