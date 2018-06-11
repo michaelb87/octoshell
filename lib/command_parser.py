e29eb2488e247df8fa5c3e8cf4ce936c33374ee9
michaelb87 <mb@michaelb.at>
2018-06-11 02:57:46 +0200

    exception handler

--- lib/command_parser.py
+++ lib/command_parser.py
@@ -5,13 +5,16 @@ class CommandParser():
 
     def parse_cmd(self,cmd):
         cleaned = cmd.strip()
-        if cleaned == 'h' or cleaned == 'help':
-            return self.help_text()
-        if cleaned == 'ls' or cleaned == 'list':
-            return self.api_manager.list_files()
-        if cleaned == 'status':
-            return self.api_manager.get_status()
-        return cmd
+        try:
+            if cleaned == 'h' or cleaned == 'help':
+                return self.help_text()
+            if cleaned == 'ls' or cleaned == 'list':
+                return self.api_manager.list_files()
+            if cleaned == 'status':
+                return self.api_manager.get_status()
+            return cmd
+        except Exception as e:
+            return 'Error: {}'.format(e) 
     
     def help_text(self):
         return dedent(
